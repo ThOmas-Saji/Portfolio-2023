009d03ef28fed9809966228a32563049ea835478
ThOmas-Saji <thomasms213@gmail.com>
2023-12-11 10:35:15 +0530

    transition adjusted in projects section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,63 +3,6 @@ import "./portfolio.scss";
 import { useScroll, useSpring, motion, useTransform } from "framer-motion";
 import { myProjects } from "./projects";
 
-const data = [
-  {
-    id: 1,
-    title: "Book Club",
-    des: "A book club webpage where user can browse books, intract with other people and talk about their favorite books",
-    ts: " React | Redux | Socket io | MongoDB | JavaScript | HTML | CSS",
-    image: "/book_club.png",
-    github: "https://github.com/sreerag-rajan/book-club",
-    live: "https://book-club-project.vercel.app/",
-  },
-  {
-    id: 2,
-    title: "Capital Movies",
-    des: "Movies webpage where user can see latest and popular movies and details, if user logged in he can create a favorite movies list",
-    ts: " React | Redux-toolkit | Nodejs | Express | MongoDB | JavaScript | CSS | Material UI",
-    image: "/CapitalMovies.png",
-    github: "https://github.com/ThOmas-Saji/Capital-Movies",
-    live: "https://capital-movies-ten.vercel.app/discover/popular",
-  },
-  {
-    id: 3,
-    title: "Online Store",
-    des: "A simple e-commerce(Dummy) web site for men, women and kids .",
-    ts: " React | Redux | Nodejs | Express | MongoDB | JavaScript | CSS | Material UI",
-    image: "/onlineStore.png",
-    github: "https://github.com/ThOmas-Saji/online-store",
-    live: "https://online-store-tau.vercel.app/",
-  },
-  {
-    id: 4,
-    title: "Mama Earth Clone",
-    des: "A website for buying various skincare products for babies and females.",
-    ts: "HTML | CSS | JavaScript",
-    image: "/MamaE.png",
-    github: "https://github.com/mehtaanand35/MamaEarthClone",
-    live: "https://eloquent-allen-ba4ad9.netlify.app/",
-  },
-  {
-    id: 5,
-    title: "Weather App 2.0",
-    des: "A simple webpage for checking the current and future weather status.",
-    ts: "React | contextapi | Bootstrap | HTML | CSS | TypeScript",
-    image: "/WA.png",
-    github: "https://github.com/ThOmas-Saji/Weather-App-2.0",
-    live: "https://weather-app-2-0-snowy.vercel.app/",
-  },
-  {
-    id: 6,
-    title: "H & M Clone",
-    des: "An e-commerce website known for its fast fashion for men,women, teenagers, and children.",
-    ts: "HTML | CSS | JavaScript",
-    image: "/HandM.png",
-    github: "https://github.com/ThOmas-Saji/H-and-M-Clone-Project",
-    live: "https://mystifying-hypatia-3f95a1.netlify.app/",
-  },
-];
-
 const SingleProject = ({ item }) => {
   const ref = useRef();
 
@@ -77,6 +20,7 @@ const SingleProject = ({ item }) => {
           <motion.div
             style={{
               x: -100,
+              transition:'all 0s'
             }}
             whileInView={{
               x: 0,
